6b7e3299fcce049256e829db0b0407faa8a370dd
UfukBiber <biberufuk96@gmail.com>
2022-06-16 19:49:37 +0300

    Update

---
+++ ConvModel_2.py
@@ -0,0 +1,71 @@
+from tensorflow.keras import layers
+import tensorflow as tf 
+
+
+VALIDATION_SPLIT = 0.2
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "TrainImages",
+    batch_size = 32,
+    image_size = (180, 180),
+    validation_split = VALIDATION_SPLIT,
+    subset = "training",
+    shuffle = True,
+    seed = 1234
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "TrainImages",
+    batch_size = 32,
+    image_size = (180, 180),
+    validation_split = VALIDATION_SPLIT,
+    subset = "validation",
+    shuffle = True,
+    seed = 1234
+)
+
+
+data_augmentation = tf.keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.2)
+])
+
+Inputs = layers.Input(shape = (180, 180, 3))
+x = data_augmentation(Inputs)
+x = layers.Rescaling(1. / 255)(x)
+x = layers.Conv2D(filters=32, kernel_size = (3, 3), activation = "relu")(x)
+x = layers.MaxPooling2D(pool_size = (2, 2))(x)
+
+x = layers.Conv2D(filters=64, kernel_size = (3, 3), activation = "relu")(x)
+x = layers.MaxPooling2D(pool_size = (2, 2))(x)
+
+x = layers.Conv2D(filters=64, kernel_size = (3, 3), activation = "relu")(x)
+x = layers.MaxPooling2D(pool_size = (2, 2))(x)
+
+x = layers.Conv2D(filters=128, kernel_size = (3, 3), activation = "relu")(x)
+x = layers.MaxPooling2D(pool_size = (2, 2))(x)
+
+x = layers.Conv2D(filters=256, kernel_size = (3, 3), activation = "relu")(x)
+x = layers.MaxPooling2D(pool_size = (2, 2))(x)
+
+x = layers.Flatten()(x)
+x = layers.Dense(1, activation = "sigmoid")(x)
+
+
+model = tf.keras.models.Model(Inputs, x)
+model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+print(model.summary())
+
+model.fit(train_ds, validation_data = val_ds, epochs = 50, 
+        callbacks = [tf.keras.callbacks.ModelCheckpoint("ConvModel", save_best_only = True, monitor = "val_accuracy")])
+
+
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    "TestImages",
+    batch_size = 32,
+    image_size = (180, 180)
+)
+
+model.evaluate(test_ds)
